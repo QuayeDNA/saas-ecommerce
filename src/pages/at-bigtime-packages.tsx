9d8dc56e07414490afe8bbdc51a13204529490da
QuayeDNA <quayedavid245@gmail.com>
2026-05-09 12:12:29 +0000

    feat: update ProviderPackageDisplay to use packageId instead of packageSlug for AT provider pages

--- src/pages/at-bigtime-packages.tsx
+++ src/pages/at-bigtime-packages.tsx
@@ -1,9 +1,12 @@
-import { ProviderPackageDisplay } from '../components/products/ProviderPackageDisplay';
+import { ProviderPackageDisplay } from "../components/products/ProviderPackageDisplay";
 
 export const AtBigTimePackagesPage = () => {
   return (
     <div className="space-y-6">
-      <ProviderPackageDisplay provider="AT" packageSlug="at-big-time" />
+      <ProviderPackageDisplay
+        provider="AT"
+        packageId="6877d5d29fc6512ec81aa957"
+      />
     </div>
   );
 };
--- src/pages/at-ishare-packages.tsx
+++ src/pages/at-ishare-packages.tsx
@@ -1,9 +1,12 @@
-import { ProviderPackageDisplay } from '../components/products/ProviderPackageDisplay';
+import { ProviderPackageDisplay } from "../components/products/ProviderPackageDisplay";
 
 export const AtISharePremiumPackagesPage = () => {
   return (
     <div className="space-y-6">
-      <ProviderPackageDisplay provider="AT" packageSlug="at-ishare-premium" />
+      <ProviderPackageDisplay
+        provider="AT"
+        packageId="6877d5da9fc6512ec81aa974"
+      />
     </div>
   );
 };
